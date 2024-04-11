d081f117def484fb312983a6c76122687838f51c
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-04-11 15:33:23 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        int[][] memo = new int[41][2];
+
+        memo[0][0] = 1;
+        memo[0][1] = 0;
+        memo[1][0] = 0;
+        memo[1][1] = 1;
+        
+        for (int i = 2; i <= 40; i++) {
+            memo[i][0] = memo[i - 1][0] + memo[i - 2][0];
+            memo[i][1] = memo[i - 1][1] + memo[i - 2][1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(memo[n][0]).append(" ").append(memo[n][1]).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
