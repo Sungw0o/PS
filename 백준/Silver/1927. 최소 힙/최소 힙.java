a4ea968b782354189d59b3a34be372ba14dd837c
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-04-29 20:18:27 +0900

    [Silver II] Title: 최소 힙, Time: 328 ms, Memory: 26492 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append("0\n");
+                } else {
+                    sb.append(pq.poll()).append('\n');
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+        System.out.println(sb);
+    }
+}
