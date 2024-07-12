5a6cd0388691dd2728e0afe02e4b0020718b8400
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-07-12 19:13:32 +0900

    다리를 지나는 트럭.java

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,32 +2,25 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 
-
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        int totalWeight = 0;
         Queue<Integer> queue = new LinkedList<>();
+        int totalWeight = 0;
         
         for (int truck : truck_weights) {
             while (true) {
-                if (queue.isEmpty()) {
+                if (queue.size() == bridge_length) {
+                    totalWeight -= queue.poll();
+                }
+                if (totalWeight + truck <= weight) {
                     queue.offer(truck);
                     totalWeight += truck;
                     answer++;
                     break;
-                } else if (queue.size() == bridge_length) {
-                    totalWeight -= queue.poll();
                 } else {
-                    if (totalWeight + truck <= weight) {
-                        queue.offer(truck);
-                        totalWeight += truck;
-                        answer++;
-                        break;
-                    } else {
-                        queue.offer(0);
-                        answer++;
-                    }
+                    queue.offer(0);
+                    answer++;
                 }
             }
         }
