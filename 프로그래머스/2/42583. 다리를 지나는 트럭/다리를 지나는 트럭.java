ddc08019c5d0f3d204dfaad1d48f111119e45f26
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-07-12 22:15:18 +0900

    Update 다리를 지나는 트럭.java

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,35 +3,27 @@ import java.util.Queue;
 
 
 class Solution {
-            public int solution(int bridge_length, int weight, int[] truck_weights) {
-                int answer = 0;
-                int totalWeight = 0;
-                Queue<Integer> queue = new LinkedList<>();
-
-                for (int truck : truck_weights) {
-                    while (true) {
-                        if (queue.isEmpty()) {
-                            queue.offer(truck);
-                            totalWeight += truck;
-                            answer++;
-                            break;
-                        } else if (queue.size() == bridge_length) {
-                            totalWeight -= queue.poll();
-                        } else {
-                            if (totalWeight + truck <= weight) {
-                                queue.offer(truck);
-                                totalWeight += truck;
-                                answer++;
-                                break;
-                            } else {
-                                queue.offer(0);
-                                answer++;
-                            }
-                        }
-                    }
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        int totalWeight = 0;
+        
+        for (int truck : truck_weights) {
+            while (true) {
+                answer++;
+                if (queue.size() == bridge_length) {
+                    totalWeight -= queue.poll();
                 }
-
-                return answer + bridge_length;
+                if (totalWeight + truck <= weight) {
+                    queue.offer(truck);
+                    totalWeight += truck;
+                    break;
+                } else {
+                    queue.offer(0);
+                }
             }
-        }
-
+        }    
+        
+        return answer + bridge_length;
+    }
+}
