df4a96d38077cdde830f032e0e578f6be65db4b2
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-07-19 22:24:13 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 18472 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int K;
+
+    static int visited[] = new int[100001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        int time = bfs(N);
+        System.out.println(time);
+
+    }
+
+    static int bfs(int node){
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        queue.add(node);
+        int index = node;
+        int n = 0;
+        visited[index] = 1;
+        while (queue.isEmpty() == false)
+        {
+            n = queue.remove();
+
+            if (n == K)
+            {
+                return visited[n]-1;
+            }
+
+            if (n-1>=0 && visited[n-1] == 0)
+            {
+                visited[n-1] = visited[n]+1;
+                queue.add(n-1);
+            }
+            if (n+1 <= 100000 && visited[n+1] == 0)
+            {
+                visited[n+1] = visited[n]+1;
+                queue.add(n+1);
+            }
+            if (2*n <= 100000 && visited[2*n] == 0)
+            {
+                visited[2*n] = visited[n] + 1;
+                queue.add(2*n);
+            }
+        }
+        return -1;
+    }
+}
