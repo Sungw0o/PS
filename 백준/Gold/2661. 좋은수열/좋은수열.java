fbfbdd252d182ab5343a5a5e643aea85532d02b9
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-07-27 16:51:04 +0900

    [Gold IV] Title: 좋은수열, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Gold/2661. 좋은수열/좋은수열.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+    static int[] sequence;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        sequence = new int[N];
+
+        backTracking(0);
+    }
+
+    static boolean backTracking(int depth) {
+        if (depth == N) {
+            printSequence();
+            return true;
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            sequence[depth] = i;
+            if (isValid(depth)) {
+                if (backTracking(depth + 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static boolean isValid(int depth) {
+        for (int len = 1; len <= (depth + 1) / 2; len++) {
+            boolean isSame = true;
+            for (int j = 0; j < len; j++) {
+                if (sequence[depth - j] != sequence[depth - len - j]) {
+                    isSame = false;
+                    break;
+                }
+            }
+            if (isSame) return false;
+        }
+        return true;
+    }
+
+    static void printSequence() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(sequence[i]);
+        }
+        System.out.println(sb.toString());
+    }
+}
