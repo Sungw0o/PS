952776a636b93c300212e595c9d1ed62fb640041
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-07-29 11:47:41 +0900

    [level 3] Title: 입국심사, Time: 109.81 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        long left = 1, right = (long) n * times[times.length - 1], answer = right;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long total = 0;
+            for (int t : times) total += mid / t;
+
+            if (total >= n) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return answer;
+    }
+}
