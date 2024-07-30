d6ce719839631efb2906ae74d96c0ecf45d25929
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-07-31 00:29:28 +0900

    [Silver II] Title: 랜선 자르기, Time: 192 ms, Memory: 17428 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,47 +1,52 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static long[] arr;
-    static long n;
+    static long K, N;
+    static long[] LAN;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int k = Integer.parseInt(input[0]);
-        n = Long.parseLong(input[1]);
-        arr = new long[k];
-        for (int i = 0; i < k; i++) {
-            arr[i] = Long.parseLong(br.readLine());
-        }
-        Arrays.sort(arr);
-        long start = 1; // 랜선의 길이는 최소 1부터 시작
-        long end = arr[k - 1];
-        long answer = 0;
 
-        while (start <= end) {
-            long mid = (start + end) / 2;
-            if (countLanCables(mid) >= n) { // 만들 수 있는 랜선의 개수가 목표보다 크거나 같으면
-                answer = mid; // 현재 길이를 기록하고
-                start = mid + 1; // 길이를 늘려서 더 큰 길이로 시도
-            } else { // 목표보다 작으면
-                end = mid - 1; // 길이를 줄여서 더 작은 길이로 시도
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        K = Long.parseLong(st.nextToken());
+        N = Long.parseLong(st.nextToken());
+
+        LAN = new long[(int)K];
+
+        for (int i = 0; i < K; i++) {
+            LAN[i] = Long.parseLong(br.readLine());
         }
 
-        System.out.println(answer);
+        Arrays.sort(LAN);
+        System.out.println(binarySearch(LAN));
+
+        br.close();
     }
 
-    // 주어진 길이로 만들 수 있는 랜선의 개수 계산
-    static long countLanCables(long length) {
-        long count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            count += arr[i] / length;
+    static long binarySearch(long[] lan) {
+        long left = 1;
+        long right = lan[(int)K - 1];
+        long maxLength = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long count = 0;
+            for (long length : lan) {
+                count += length / mid;
+            }
+
+            if (count >= N) {
+                maxLength = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        return count;
+        return maxLength;
     }
 }
