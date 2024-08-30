3820062c5d1e2f4419fc481c597898c37227f714
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-08-30 15:07:07 +0900

    Update 등굣길.java

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,6 +1,28 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        return answer;
+        int[][] dp = new int[n + 1][m + 1];
+        
+
+        dp[1][1] = 1;
+        
+        for (int[] puddle : puddles) {
+            dp[puddle[1]][puddle[0]] = -1;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (dp[i][j] == -1 || (i == 1 && j == 1)) {
+                    if (dp[i][j] == -1) dp[i][j] = 0; 
+                    continue;
+                }
+                
+                if (i > 1) dp[i][j] += dp[i - 1][j];
+                if (j > 1) dp[i][j] += dp[i][j - 1];
+                
+                dp[i][j] %= 1000000007;
+            }
+        }
+        
+        return dp[n][m];
     }
 }
