910c35a9db127a52851682e10adaef634fffb669
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2024-09-13 15:23:21 +0900

    Update N－Queen.java

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class n9663 {
     static int N;
     static int[][] map;
     static int cnt = 0;
@@ -25,15 +25,15 @@ public class Main {
         }
 
         for (int y = 0; y < N; y++) {
-            if (isSafe(y, x)) {
+            if (isattack(y, x)) {
                 map[y][x] = 1;
                 placeQueen(x + 1);
                 map[y][x] = 0;
             }
         }
     }
-    
-    public static boolean isSafe(int y, int x) {
+
+    public static boolean isattack(int y, int x) {
         for (int i = 0; i < x; i++) {
             if (map[y][i] == 1) return false;
             if (y - (x - i) >= 0 && map[y - (x - i)][i] == 1) return false;
