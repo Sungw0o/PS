a65ee3968cae14ccdf6804ad502d34b176b8d2f4
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2024-09-23 13:24:03 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 66.9 MB, 시간: 1.19 ms
+메모리: 79.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 23일 12:21:42
+2024년 09월 23일 13:23:59
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,35 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        
-        int answer = n - lost.length;
-        
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        for (int i = 0; i < lost.length; i++) {
-			for (int j = 0; j < reserve.length; j++) {
-				if (lost[i] == reserve[j]) {
-					answer++;
-					lost[i] = -1;
-					reserve[j] = -1;
-                    break;
-				}
-			}
-		}
-        
-        for (int i = 0; i < lost.length; i++) {
-			for (int j = 0; j < reserve.length; j++) {
-				if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
-					answer++;
-					reserve[j] = -1;
-					break;
-				}
-			}
-		}
-        
+        int[] people = new int[n];
+        int answer = n;
+
+        for (int l : lost) 
+            people[l-1]--;
+        for (int r : reserve) 
+            people[r-1]++;
+
+        for (int i = 0; i < people.length; i++) {
+            if(people[i] == -1) {
+                if(i-1>=0 && people[i-1] == 1) {
+                    people[i]++;
+                    people[i-1]--;
+                }else if(i+1< people.length && people[i+1] == 1) {
+                    people[i]++;
+                    people[i+1]--;
+                }else 
+                    answer--;
+            }
+        }
         return answer;
     }
 }
-    
