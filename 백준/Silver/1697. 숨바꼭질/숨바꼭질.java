1e976b6c2a1536c92e803e3bb3fafcd83c41a1b6
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2024-10-08 14:13:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 18372 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -6,54 +6,51 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, K;
+    static int[] visited = new int[100001];
 
-    static int N;
-    static int K;
-
-    static int visited[] = new int[100001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         int time = bfs(N);
         System.out.println(time);
-
+        br.close();
     }
 
-    static int bfs(int node){
-        Queue<Integer> queue = new LinkedList<Integer>();
+    static int bfs(int index) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(index);
+        visited[index] = 1; // 시작 위치 방문 처리
 
-        queue.add(node);
-        int index = node;
-        int n = 0;
-        visited[index] = 1;
-        while (queue.isEmpty() == false)
-        {
-            n = queue.remove();
+        while (!q.isEmpty()) {
+            int d = q.poll();
 
-             if (n == K)
-            {
-                return visited[n]-1;
+            // 목표 위치에 도달한 경우
+            if (d == K) {
+                return visited[d] - 1; // 0초부터 시작하므로 -1
             }
 
+            // d - 1
+            if (d - 1 >= 0 && visited[d - 1] == 0) {
+                visited[d - 1] = visited[d] + 1;
+                q.add(d - 1);
+            }
 
-            if (n-1>=0 && visited[n-1] == 0)
-            {
-                visited[n-1] = visited[n]+1;
-                queue.add(n-1);
+            // d + 1
+            if (d + 1 <= 100000 && visited[d + 1] == 0) {
+                visited[d + 1] = visited[d] + 1;
+                q.add(d + 1);
             }
-            if (n+1 <= 100000 && visited[n+1] == 0)
-            {
-                visited[n+1] = visited[n]+1;
-                queue.add(n+1);
-            }
-            if (2*n <= 100000 && visited[2*n] == 0)
-            {
-                visited[2*n] = visited[n] + 1;
-                queue.add(2*n);
+
+            // 2 * d
+            if (2 * d <= 100000 && visited[2 * d] == 0) {
+                visited[2 * d] = visited[d] + 1;
+                q.add(2 * d);
             }
         }
-        return -1;
+        return -1; // 도달할 수 없는 경우
     }
 }
