61cd526c95f84c185de058084c93631922db2b54
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2024-11-19 23:48:46 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -17,8 +17,12 @@ public class Main {
         for(int i = 4; i <= N; i++) {
             dp[i] = Math.min(dp[i-1], dp[i-3])+1;
         }
-        System.out.println(dp[N] % 2 == 0 ? "CY": "SK");
-
+        if(dp[N] % 2 == 0){
+            System.out.println("CY");
+        }else{
+            System.out.println("SK");
+        }
+        
         br.close();
     }
 }
