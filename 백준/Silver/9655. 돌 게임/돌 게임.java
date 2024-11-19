fa405bdad1b0bcc9a0a63160f37a6f4935233e7f
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2024-11-19 23:48:26 +0900

    Update 돌 게임.java

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -17,12 +17,8 @@ public class Main {
         for(int i = 4; i <= N; i++) {
             dp[i] = Math.min(dp[i-1], dp[i-3])+1;
         }
-        if(dp[N] % 2 == 0){
-            System.out.println("CY");
-        }else{
-            System.out.println("SK");
-        }
-        
+        System.out.println(dp[N] % 2 == 0 ? "CY": "SK");
+
         br.close();
     }
 }
