1128548afed12cbe646481ff6a0e299eaa5536c8
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2024-12-06 00:57:48 +0900

    [Gold IV] Title: 여행 가자, Time: 168 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static ArrayList<Integer>[] cities;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        cities = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            cities[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                int connected = Integer.parseInt(st.nextToken());
+                if (connected == 1) {
+                    cities[i].add(j);
+                }
+            }
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] plan = new int[M];
+        for (int i = 0; i < M; i++) {
+            plan[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(plan[0]);
+
+        boolean possible = true;
+        for (int city : plan) {
+            if (!visited[city]) {
+                possible = false;
+                break;
+            }
+        }
+
+        System.out.println(possible ? "YES" : "NO");
+        br.close();
+    }
+
+
+    private static void dfs(int city) {
+        visited[city] = true;
+        for (int nextCity : cities[city]) {
+            if (!visited[nextCity]) {
+                dfs(nextCity);
+            }
+        }
+    }
+}
