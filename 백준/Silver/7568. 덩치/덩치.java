19d316d16545f810a5e6e3fdeb9fe3846b502cef
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2024-12-20 18:00:20 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,26 +7,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        Person[] people = new Person[n]; 
-
+        Person[] people = new Person[n];
+        
         for (int i = 0; i < n; i++) {
             String[] perInfo = br.readLine().split(" ");
             int height = Integer.parseInt(perInfo[0]);
             int weight = Integer.parseInt(perInfo[1]);
             people[i] = new Person(height, weight);
         }
-
+        
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             int rank = 1;
             for (int j = 0; j < n; j++) {
-                if (i != j && people[i].height < people[j].height && people[i].weight < people[j].weight) {
+                if (i != j && people[i].isSmallerThan(people[j])) {
                     rank++;
                 }
             }
-            System.out.print(rank + " ");
+            sb.append(rank).append(" ");
         }
+        
+        System.out.println(sb);
+        br.close();
     }
-
+    
     static class Person {
         int height;
         int weight;
@@ -36,6 +38,10 @@ public class Main {
         public Person(int height, int weight) {
             this.height = height;
             this.weight = weight;
+        }
+
+        public boolean isSmallerThan(Person other) {
+            return this.height < other.height && this.weight < other.weight;
         }
     }
 }
