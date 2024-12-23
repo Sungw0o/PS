ad622954acbff14ad1449285921e78863b3abfce
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2024-12-23 11:44:12 +0900

    Update 한 줄로 서기.java

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -7,33 +7,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine()); 
+        int[] tall = new int[N+1]; 
+        ArrayList<Integer> ans = new ArrayList<>(); 
 
-        int[] heights = new int[N]; 
         String[] input = br.readLine().split(" ");
-        for (int i = 0; i < N; i++) {
-            heights[i] = Integer.parseInt(input[i]);
-        }
-
-        int[] result = new int[N]; 
-
-        for (int i = 0; i < N; i++) {
-            int count = heights[i];
-            int index = 0; 
-            
-            while (count > 0 || result[index] != 0) {
-                if (result[index] == 0) { 
-                    count--;
-                }
-                index++;
-            }
-
-            result[index] = i + 1; 
+        for (int i = 1; i <= N; i++) {
+            tall[i] = Integer.parseInt(input[i - 1]);
         }
         
-        for (int i = 0; i < N; i++) {
-            System.out.print(result[i] + " ");
+        for (int i = N; i >= 1; i--) {
+            ans.add(tall[i], i); 
         }
-        
-        br.close();
+
+        for (int k : ans) {
+            System.out.print(k + " ");
+        }
+
+        br.close(); 
     }
 }
