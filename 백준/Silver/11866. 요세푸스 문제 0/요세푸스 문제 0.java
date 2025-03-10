9174fee1039b8197528bf366fbeaf8c6eacdad5d
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2025-03-10 23:38:27 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 148 ms, Memory: 28064 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,15 +1,16 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt(); 
-        int K = scanner.nextInt(); 
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
         Queue<Integer> queue = new LinkedList<>();
         List<Integer> result = new ArrayList<>();
 
-   
         for (int i = 1; i <= N; i++) {
             queue.add(i);
         }
@@ -17,22 +18,24 @@ public class Main {
         int count = 0;
         while (!queue.isEmpty()) {
             count++;
-            int person = queue.poll(); 
+            int person = queue.poll();
             if (count % K == 0) {
-                result.add(person); 
+                result.add(person);
             } else {
-                queue.add(person); 
+                queue.add(person);
             }
         }
 
-    
-        System.out.print("<");
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
         for (int i = 0; i < result.size(); i++) {
-            System.out.print(result.get(i));
+            sb.append(result.get(i));
             if (i < result.size() - 1) {
-                System.out.print(", ");
+                sb.append(", ");
             }
         }
-        System.out.println(">");
+        sb.append(">");
+        
+        System.out.println(sb.toString());
     }
 }
