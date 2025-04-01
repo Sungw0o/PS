1a64f4688138c1e056e232eb977495ce74422eba
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2025-04-01 19:03:49 +0900

    [Silver II] Title: 점프 점프, Time: 120 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(jump(n, arr));
+        br.close();
+    }
+
+    private static int jump(final int n, final int[] arr) {
+        if (n == 1) {
+            return 0;
+        }
+        if (arr[0] == 0) {
+            return -1;
+        }
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < n; i++) {
+            farthest = Math.max(farthest, i + arr[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+
+                if (currentEnd >= n - 1) {
+                    return jumps;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
