66bb1360f7f247dcbf06282f950ed9e5e0859df6
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2025-07-16 16:33:47 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 53540 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,30 +1,28 @@
-
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        Deque<Integer> queue = new LinkedList<>();
-        for(int i=1;i<=n;i++){
-            queue.add(i);
-        }
-        while(true){
-            int count = queue.size();
-            if(count==1){
-                System.out.println(queue.getFirst());
-                break;
-            }
-            queue.pollFirst();
-            int k = queue.getFirst();
-            queue.addLast(k);
-            queue.removeFirst();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> cards = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            cards.addLast(i);
         }
+
+        while(cards.size() > 1) {
+            cards.removeFirst();
+            int temp = cards.removeFirst();
+            cards.addLast(temp);
+        }
+
+        System.out.println(cards.removeFirst());
+        br.close();
     }
 }
