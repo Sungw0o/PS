41258fe979c8acb6577530caa653b9bd5299450f
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2025-07-23 17:37:13 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 604 ms, Memory: 142060 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,58 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visited;
-    static ArrayList<Integer>[] nodeList;
+    private static List<Integer>[] graph;
+    private static boolean[] visited;
+    private static int ll = 0;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] NM = br.readLine().split(" ");
-        int n = Integer.parseInt(NM[0]);
-        int m = Integer.parseInt(NM[1]);
-        nodeList = new ArrayList[n+1];
-        for(int i=0;i<=n;i++){
-            nodeList[i] = new ArrayList<>();
-        }
-        for(int i=0;i<m;i++){
-            String[] UV = br.readLine().split(" ");
-            int u = Integer.parseInt(UV[0]);
-            int v = Integer.parseInt(UV[1]);
-            nodeList[u].add(v);
-            nodeList[v].add(u);
-        }
-        int num = getConnectedComponent(n,nodeList);
-        System.out.println(num);
-    }
-    static int getConnectedComponent(int N, ArrayList<Integer>[] g) {
-        nodeList = g;
-        visited = new boolean[N + 1];
-        int ans = 0;
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        graph = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        
+        visited = new boolean[N + 1];
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
-                bfs(i);
-                ans++;
+                dfs(i);
+                ll++;
             }
         }
-        return ans;
+        
+        sb.append(ll);
+        System.out.println(sb);
+        br.close();
     }
-
-
-    static void bfs(int v){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(v);
+    
+    public static void dfs(int v) {
         visited[v] = true;
-
-        while (!queue.isEmpty()){
-            int node = queue.poll();
-            for (int next : nodeList[node]){
-                if(!visited[next]){
-                    queue.offer(next);
-                    visited[next] = true;
-                }
+        for (int w : graph[v]) {
+            if (!visited[w]) {
+                dfs(w);
             }
         }
     }
-
-
 }
