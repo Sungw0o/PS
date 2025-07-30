457cd90f48eb61d39f529247b961cfcc8cf129e3
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2025-07-30 23:16:55 +0900

    [Silver III] Title: 소수 구하기, Time: 420 ms, Memory: 19052 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,24 +1,36 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        for(int i=x; i<=y; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        for(int i = M; i <= N; i++){
             if(isPrime(i)){
-                sb.append(i).append('\n');
+                sb.append(i).append("\n");
             }
         }
-        System.out.println(sb.toString());
+
+        System.out.println(sb);
+        br.close();
     }
 
-    public static boolean isPrime(int number){
-        if(number < 2) return false;
-        for(int j=2; j*j<=number; j++){
-            if(number % j == 0) return false;
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+
+        for (int j = 2; j * j <= n; j++) {
+            if (n % j == 0) {
+                return false;
+            }
         }
         return true;
     }
