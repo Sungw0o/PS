73485356f8b67777ff063b81e36e310227ba2123
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2025-08-05 17:16:40 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1172 ms, Memory: 254172 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static List<Integer>[] graph;
+    private static int N, M, K, X;
+    private static int[] distance;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            graph[from].add(to);
+        }
+
+        distance = new int[N + 1];
+        Arrays.fill(distance, -1);
+        bfs(X);
+
+        boolean found = false;
+        for (int i = 1; i <= N; i++) {
+            if (distance[i] == K) {
+                System.out.println(i);
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println(-1);
+        }
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        distance[start] = 0;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for (int next : graph[current]) {
+                if (distance[next] == -1) {
+                    distance[next] = distance[current] + 1;
+                    queue.add(next);
+                }
+            }
+        }
+    }
+}
