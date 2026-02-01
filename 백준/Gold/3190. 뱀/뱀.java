05066f9681b07dc1dad98ddd2255b63b4a76c1b1
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-01 19:58:45 +0900

    [Gold IV] Title: 뱀, Time: 108 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -2,75 +2,86 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static int N, K, L;
+    private static int[][] map;
+    private static boolean[][] isBody;
+    private static Map<Integer, Character> dirInfo = new HashMap<>();
+
+    private static int[] dx = {0, 1, 0, -1};
+    private static int[] dy = {1, 0, -1, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        int K = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
 
-        int[][] map = new int[N][N];
+        map = new int[N][N];
+        isBody = new boolean[N][N];
 
         for (int i = 0; i < K; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int row = Integer.parseInt(st.nextToken()) - 1;
-            int col = Integer.parseInt(st.nextToken()) - 1;
-            map[row][col] = 1;
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken()) - 1;
+            map[r][c] = 1;
         }
 
-        int L = Integer.parseInt(br.readLine());
-        Queue<int[]> directions = new LinkedList<>();
-
+        L = Integer.parseInt(br.readLine());
         for (int i = 0; i < L; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int time = Integer.parseInt(st.nextToken());
             char dir = st.nextToken().charAt(0);
-            directions.add(new int[]{time, dir});
+            dirInfo.put(time, dir);
         }
 
-        int[][] moves = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int direction = 0;
+        solve();
+        br.close();
+    }
 
-        Deque<int[]> snake = new LinkedList<>();
+    private static void solve() {
+        Deque<int[]> snake = new ArrayDeque<>();
         snake.add(new int[]{0, 0});
-        boolean[][] visited = new boolean[N][N];
-        visited[0][0] = true;
+        isBody[0][0] = true;
 
         int time = 0;
+        int dir = 0;
 
         while (true) {
             time++;
-            int[] head = snake.peekLast();
-            int nx = head[0] + moves[direction][0];
-            int ny = head[1] + moves[direction][1];
 
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny]) {
+            int[] head = snake.peekLast();
+            int nx = head[0] + dx[dir];
+            int ny = head[1] + dy[dir];
+
+            if (isOutOfBounds(nx, ny) || isBody[nx][ny]) {
                 break;
             }
 
             snake.addLast(new int[]{nx, ny});
-            
-            visited[nx][ny] = true;
+            isBody[nx][ny] = true;
 
             if (map[nx][ny] == 1) {
                 map[nx][ny] = 0;
-            } 
-            else {
+            } else {
                 int[] tail = snake.pollFirst();
-                visited[tail[0]][tail[1]] = false;
+                isBody[tail[0]][tail[1]] = false;
             }
 
-            if (!directions.isEmpty() && directions.peek()[0] == time) {
-                int[] change = directions.poll();
-                if (change[1] == 'D') {
-                    direction = (direction + 1) % 4;
-                } 
-                else {
-                    direction = (direction + 3) % 4;
+            if (dirInfo.containsKey(time)) {
+                char turn = dirInfo.get(time);
+                if (turn == 'D') {
+                    dir = (dir + 1) % 4;
+                } else {
+                    dir = (dir + 3) % 4;
                 }
             }
         }
 
         System.out.println(time);
-        br.close();
+    }
+
+    private static boolean isOutOfBounds(int r, int c) {
+        return r < 0 || c < 0 || r >= N || c >= N;
     }
 }
