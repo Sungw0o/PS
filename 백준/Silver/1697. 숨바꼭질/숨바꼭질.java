28c4b77c27022cd7a269432632e4ebc672eab120
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-03 15:56:50 +0900

    [Silver I] Title: 숨바꼭질, Time: 120 ms, Memory: 23440 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -6,51 +6,54 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
-    static int[] visited = new int[100001];
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        int time = bfs(N);
-        System.out.println(time);
-        br.close();
-    }
-
-    static int bfs(int index) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(index);
-        visited[index] = 1; // 시작 위치 방문 처리
-
-        while (!q.isEmpty()) {
-            int d = q.poll();
-
-            // 목표 위치에 도달한 경우
-            if (d == K) {
-                return visited[d] - 1; // 0초부터 시작하므로 -1
-            }
-
-            // d - 1
-            if (d - 1 >= 0 && visited[d - 1] == 0) {
-                visited[d - 1] = visited[d] + 1;
-                q.add(d - 1);
-            }
-
-            // d + 1
-            if (d + 1 <= 100000 && visited[d + 1] == 0) {
-                visited[d + 1] = visited[d] + 1;
-                q.add(d + 1);
-            }
-
-            // 2 * d
-            if (2 * d <= 100000 && visited[2 * d] == 0) {
-                visited[2 * d] = visited[d] + 1;
-                q.add(2 * d);
-            }
-        }
-        return -1; // 도달할 수 없는 경우
-    }
+	
+	private static Queue<Integer> queue = new LinkedList<>();
+	private static int N, K;
+	
+	private static int[] map = new int[100001];
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		if(N==K) {
+			System.out.println(0);
+			return;
+		}
+		
+		int minT = bfs(N);
+		System.out.println(minT);
+		br.close();
+		
+	}
+	
+	public static int bfs(int n) {
+		queue.add(n);
+		map[n] = 1;
+				
+		while(!queue.isEmpty()) {
+			
+			int cur = queue.poll();
+			
+			if(cur == K) {
+				return map[cur] -1 ;
+			}
+			
+			int[] nextMoves = {cur - 1, cur + 1, cur * 2};
+			
+			for (int next : nextMoves) {
+				
+				if (next >= 0 && next <= 100000 && map[next] == 0) {
+					map[next] = map[cur] + 1; 
+					queue.add(next);
+				}
+			}
+		}				
+		return -1;
+	}
 }
