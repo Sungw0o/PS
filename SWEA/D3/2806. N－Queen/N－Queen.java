fcee17c7fd46eb85df0d81805c91421884c16103
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-10 11:29:05 +0900

    [D3] Title: N-Queen, Time: 97 ms, Memory: 27,136 KB -BaekjoonHub

---
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+	
+	private static int T,N,cnt;
+	private static int[][] map;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t<=T;t++) {
+			N = Integer.parseInt(br.readLine());
+			cnt = 0;
+			map = new int[N][N];
+			placeQueen(0);
+			sb.append("#"+t+" ").append(cnt).append("\n");
+	
+		}
+		
+		System.out.println(sb);
+		br.close();
+		
+		
+	}
+	
+	 public static void placeQueen(int x) {
+	        if (x == N) {
+	            cnt++;
+	            return;
+	        }
+
+	        for (int y = 0; y < N; y++) {
+	            if (isSafe(y, x)) {
+	                map[y][x] = 1;
+	                placeQueen(x + 1);
+	                map[y][x] = 0;
+	            }
+	        }
+	    }
+	    
+	    public static boolean isSafe(int y, int x) {
+	        for (int i = 0; i < x; i++) {
+	            if (map[y][i] == 1) return false;
+	            if (y - (x - i) >= 0 && map[y - (x - i)][i] == 1) return false;
+	            if (y + (x - i) < N && map[y + (x - i)][i] == 1) return false;
+	        }
+
+	        return true;
+	    }
+}
