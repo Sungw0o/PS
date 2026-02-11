b93db94f0e7fef97dc999280d845511a93b15a24
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-11 11:43:12 +0900

    [D3] Title: 농작물 수확하기, Time: 87 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,74 +1,57 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution
-{
-    public static void main(String args[]) throws Exception
-    {
-
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
-
-
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            int n = sc.nextInt();
-            int[][] arr = new int[n][n];
-            for(int i = 0; i < n; i++){
-                String line = sc.next();
-                for(int j = 0; j < n; j++){
-                    arr[i][j] = line.charAt(j) - '0';
-                }
-            }
-            System.out.println("#"+test_case+" "+farm(arr,n));
-        }
-    }
-
-    public static int farm(int[][] arr,int n){
-        int rowsum = 0;
-        int lfp = n /2;
-        int rfp = n /2;
-        for(int i = 0;i<= n;i++){
-            for(int j = lfp;j<=rfp;j++){
-                rowsum += arr[i][j];
-            }
-
-            if(i < n/2){
-                lfp--;
-                rfp++;
-            }
-            else{
-                lfp++;
-                rfp--;
-            }
-        }
-        return rowsum;
-    }
+public class Solution {
+	
+	private static int T,N,profit;
+	
+	private static char[][] farm;
+	
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	
+	public static void main(String[] args) throws Exception {
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t<= T ; t++) {
+			
+			init();
+			calc();
+			sb.append("#"+t+" ").append(profit).append("\n");
+			
+		}
+		
+		System.out.println(sb);
+		br.close();
+	}
+	
+	public static void init() throws Exception, IOException {
+		
+		N = Integer.parseInt(br.readLine());
+		profit = 0;
+		farm = new char[N][N];
+		
+		for(int i = 0 ; i< N; i++) {
+			st = new StringTokenizer(br.readLine());
+			farm[i] = st.nextToken().toCharArray();
+		}
+	}
+	
+	public static void calc() {
+		
+		for(int i = 0 ; i < N ; i++) {
+			int m = N / 2 ;
+			int distance = Math.abs(i-m);
+			
+			for(int j = distance ; j < N - distance; j++) {
+				profit += farm[i][j] - '0';
+			}
+		}				
+	}
 }
