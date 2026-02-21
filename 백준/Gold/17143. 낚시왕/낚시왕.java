89c5fde1f1740dc80644eb7d8a8dff66b1fbb937
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-22 00:24:37 +0900

    [Gold I] Title: 낚시왕, Time: 388 ms, Memory: 25484 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,7 +18,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -118,11 +115,8 @@ public class Main {
                 }
             }
         }
-
-        sb.append(sum).append("\n");
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        sb.append(sum);
+        System.out.println(sb);
         br.close();
     }
 }
