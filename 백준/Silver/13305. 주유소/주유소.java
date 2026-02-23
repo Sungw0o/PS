86e0543d91e749e864af63834e5625af60f4444a
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-23 23:39:40 +0900

    [Silver III] Title: 주유소, Time: 332 ms, Memory: 35508 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -4,32 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    private static int N;
+
+    private static int[] roadLen;
+    private static int[] city;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        int[] roadlen = new int[N - 1]; 
-        int[] city = new int[N]; 
-        
+        roadLen = new int[N-1];
+        city = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N - 1; i++) {
-            roadlen[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N-1; i++) {
+            roadLen[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             city[i] = Integer.parseInt(st.nextToken());
         }
 
-        long cost = 0; 
-        long minPrice = city[0]; 
-
-        for (int i = 0; i < N - 1; i++) {
-            if (city[i] < minPrice) {
+        long cost = 0;
+        long minPrice = city[0];
+        
+        for(int i = 0; i < N-1; i++) {
+            if(city[i] < minPrice) {
                 minPrice = city[i];
             }
-            cost += minPrice * roadlen[i];
+            cost += minPrice*roadLen[i];
         }
 
         System.out.println(cost);
