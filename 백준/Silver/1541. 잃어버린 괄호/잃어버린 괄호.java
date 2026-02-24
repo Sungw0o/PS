e5d0e4b03207debef5bfcb6291fd477c1e221930
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-24 11:02:40 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 92 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,29 +1,36 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int sum = Integer.MAX_VALUE;	
-
-        StringTokenizer subtraction = new StringTokenizer(br.readLine(), "-");
-
-        while (subtraction.hasMoreTokens()) {
-            int temp = 0;
-
-            StringTokenizer addition = new StringTokenizer(subtraction.nextToken(), "+");
-            while (addition.hasMoreTokens()) {
-                temp += Integer.parseInt(addition.nextToken());
-            }
-
-            if (sum == Integer.MAX_VALUE) {
-                sum = temp;  
-            } else {
-                sum -= temp; 
-            }
-        }
-
-        System.out.println(sum);
-        br.close();
-    }
+public class Main  {
+	
+	private static int minValue = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer sub = new StringTokenizer(br.readLine(),"-");
+		
+		while(sub.hasMoreTokens()) {
+			
+			int temp = 0;
+			
+			StringTokenizer add = new StringTokenizer(sub.nextToken(),"+");
+			while(add.hasMoreTokens()) {
+				temp += Integer.parseInt(add.nextToken());
+			}	
+				if(minValue == Integer.MAX_VALUE) {
+					minValue = temp;
+				}
+				else {
+					minValue -= temp;
+				}		
+		}
+		
+		sb.append(minValue);
+		System.out.println(sb);
+		br.close();
+			
+	}
 }
