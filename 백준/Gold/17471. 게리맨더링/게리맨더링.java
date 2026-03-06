1c86075b81ecc6c4ce0ef9c0585a305b6b313eab
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-03-06 15:22:58 +0900

    [Gold III] Title: 게리맨더링, Time: 132 ms, Memory: 16052 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,114 +1,125 @@
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static int N, minDiff;
+    private static ArrayList<Integer>[] graph;
+    private static int[] population;
+    private static boolean[] isSelected;
+    
+    private static StringBuilder sb;
 
-	private static int N;
-	private static int minDiff = Integer.MAX_VALUE;
-	
-	private static int[] population;
-	
-	private static ArrayList<Integer>[] area;
-	
-	private static boolean[] isSelected; 
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		N = Integer.parseInt(br.readLine());
-
-		population = new int[N + 1];
-		area = new ArrayList[N + 1];
-		isSelected = new boolean[N + 1];
-
-		for (int i = 1; i <= N; i++) {
-			area[i] = new ArrayList<>();
-		}
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			population[i] = Integer.parseInt(st.nextToken());
-		}
-
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int count = Integer.parseInt(st.nextToken());
-			for (int j = 0; j < count; j++) {
-				int neighbor = Integer.parseInt(st.nextToken());
-				area[i].add(neighbor);
-			}
-		}
-
-		subset(1);
-		
-		System.out.println(minDiff == Integer.MAX_VALUE ? -1 : minDiff);
-		br.close();
-	}
-
-	private static void subset(int cnt) {
-		if (cnt == N + 1) {
-			check();
-			return;
-		}
-
-		isSelected[cnt] = true;
-		subset(cnt + 1);
-
-		isSelected[cnt] = false;
-		subset(cnt + 1);
-	}
-
-	private static void check() {
-		List<Integer> listA = new ArrayList<>();
-		List<Integer> listB = new ArrayList<>();
-
-		for (int i = 1; i <= N; i++) {
-			if (isSelected[i]) listA.add(i);
-			else listB.add(i);
-		}
-
-		if (listA.size() == 0 || listB.size() == 0) return;
-
-		if (isConnected(listA) && isConnected(listB)) {
-			int sumA = 0;
-			int sumB = 0;
-
-			for (int node : listA) sumA += population[node];
-			for (int node : listB) sumB += population[node];
-
-			minDiff = Math.min(minDiff, Math.abs(sumA - sumB));
-		}
-	}
-
-	private static boolean isConnected(List<Integer> list) {
-		Queue<Integer> q = new LinkedList<>();
-		boolean[] visited = new boolean[N + 1];
-
-		
-		int start = list.get(0);
-		q.offer(start);
-		visited[start] = true;
-		
-		int count = 1; 
-
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-
-			for (int next : area[cur]) {
-				if (list.contains(next) && !visited[next]) {
-					visited[next] = true;
-					q.offer(next);
-					count++;
-				}
-			}
-		}
-		return count == list.size();
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        
+        N = Integer.parseInt(br.readLine());
+        
+        graph = new ArrayList[N + 1];
+        population = new int[N + 1];
+        isSelected = new boolean[N + 1];
+        minDiff = Integer.MAX_VALUE;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+            population[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int adjCnt = Integer.parseInt(st.nextToken());
+            
+            for (int j = 1; j <= adjCnt; j++) {
+                int u = Integer.parseInt(st.nextToken());
+                graph[i].add(u);
+            }
+        }
+        
+        divide(1);
+        
+       
+        minDiff = minDiff == Integer.MAX_VALUE ? -1 : minDiff;
+        sb.append(minDiff);
+        
+        System.out.println(sb);
+        br.close();
+    }
+    
+    public static void divide(int idx) {
+    	
+    	if(idx == N + 1) {
+    		ArrayList<Integer> aList = new ArrayList<>();
+    		ArrayList<Integer> bList = new ArrayList<>();
+    		
+    		for(int i = 1; i<= N; i++) {
+    			if(isSelected[i]) aList.add(i);
+    			else bList.add(i);
+    		}
+    		
+    		if(aList.isEmpty() || bList.isEmpty()) return;
+    		
+    		if(isConnected(aList) && isConnected(bList)) {
+    			
+    			int aSum = 0;
+    			int bSum = 0;
+    			
+    			for(int i : aList) {
+    				aSum += population[i];
+    			}
+    			
+    			for(int i : bList) {
+    				bSum += population[i];
+    			}
+    			
+    			minDiff = Math.min(minDiff, Math.abs(aSum-bSum));
+    		}
+    		return;
+    	}
+        
+        isSelected[idx] = true;
+        divide(idx + 1);
+        isSelected[idx] = false;
+        divide(idx + 1);
+        
+       
+    }
+    
+    public static boolean isConnected(ArrayList<Integer> list) {
+    	
+    	if(list.isEmpty()) return false;
+    	
+    	Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[N + 1];
+        
+        int start = list.get(0);
+        q.add(start);
+        visited[start] = true;
+        
+        int count = 1;
+        
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            
+            for (int next : graph[cur]) {
+                if (!visited[next] && list.contains(next)) {
+                    visited[next] = true;
+                    q.add(next);
+                    count++;
+                }
+            }
+        }
+        
+        return count == list.size();
+    }
 }
