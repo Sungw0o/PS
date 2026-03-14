75857e6cee312caa2c4aaae283cccbf2bbde4493
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-03-14 20:46:16 +0900

    [Gold V] Title: 집합의 표현, Time: 384 ms, Memory: 47044 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,68 +1,80 @@
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int N, M;
-    private static int[] parent;
 
+    private static int N,M;
+
+    private static int[] parents;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        makeSet(N);
+        makeset(N);
 
-        for(int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int command = Integer.parseInt(st.nextToken());
+            int cmd  = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if(command == 0) {
-                union(a, b);
-            } else if(command == 1) {
-                if(find(a) == find(b)) {
-                    sb.append("YES\n");
-                } else {
-                    sb.append("NO\n");
-                }
+            if(cmd==0){
+
+                union(a,b);
             }
+
+            else if(cmd==1){
+                String str =  find(a) == find(b) ? "YES" : "NO";
+
+                sb.append(str).append("\n");
+            }
+
         }
-        
         System.out.println(sb);
         br.close();
     }
 
-    public static void makeSet(int n) {
-        parent = new int[n + 1];
-        for(int i = 0; i <= n; i++) {
-            parent[i] = i;
+    public static void makeset(int n){
+
+        parents = new int[n+1];
+
+        for(int i = 1; i <= n; i++){
+            parents[i] = i;
         }
+
     }
 
-    public static int find(int x) {
-        if(parent[x] == x) {
+    public static int find(int x){
+
+        if(parents[x] == x){
             return x;
         }
-        return parent[x] = find(parent[x]);
+
+        return parents[x] = find(parents[x]);
     }
 
-    public static boolean union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
+    public static boolean union(int a, int b){
 
-        if(rootA == rootB) {
+        int rA = find(a);
+        int rB = find(b);
+
+        if(rA == rB){
             return false;
         }
 
-        parent[rootB] = rootA;
+        parents[rB] = rA;
+
         return true;
+
+
+
     }
 }
