e5c290b709ca2753ea44222a68d4f81474465a57
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-03-14 19:48:07 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1328 ms, Memory: 336196 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,62 +1,96 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    private static List<Integer>[] graph;
-    private static int N, M, K, X;
-    private static int[] distance;
 
+    private static int N,M,K,X;
+
+    private static int[] dist;
+    private static ArrayList<ArrayList<Node>> graph;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) {
-            graph[i] = new ArrayList<>();
+
+        dist = new int[N+1];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        graph = new ArrayList<>();
+        for(int i = 0; i <= N; i++){
+            graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < M; i++) {
+        for(int i = 1; i <= M; i++){
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            graph[from].add(to);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(new Node(b,1));
         }
 
-        distance = new int[N + 1];
-        Arrays.fill(distance, -1);
         bfs(X);
 
-        boolean found = false;
-        for (int i = 1; i <= N; i++) {
-            if (distance[i] == K) {
-                System.out.println(i);
-                found = true;
+
+        int cnt = 0;
+        for(int i = 1; i <= N; i++){
+            if(dist[i] == K){
+                sb.append(i).append("\n");
+                cnt++;
             }
         }
 
-        if (!found) {
-            System.out.println(-1);
+        if(cnt == 0){
+            sb.append("-1");
         }
+
+        System.out.println(sb);
     }
 
-    private static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        distance[start] = 0;
+    public static void bfs(int s){
+        PriorityQueue<Node> q = new PriorityQueue<>();
 
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
+        q.add(new Node(s,0));
+        dist[s] = 0;
 
-            for (int next : graph[current]) {
-                if (distance[next] == -1) {
-                    distance[next] = distance[current] + 1;
-                    queue.add(next);
+
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+
+            if(dist[cur.to] < cur.cost){
+                continue;
+            }
+
+
+            for(Node next : graph.get(cur.to)){
+
+                if(dist[next.to] > dist[cur.to] + next.cost){
+                    dist[next.to] = dist[cur.to] + next.cost;
+                    q.add(new  Node(next.to, dist[next.to]));
                 }
             }
+        }
+
+
+    }
+
+    public static class Node implements Comparable<Node>{
+        int to;
+        int cost;
+        public Node(int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
         }
     }
 }
