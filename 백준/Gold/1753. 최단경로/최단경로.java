e50c67ecbca9502ee946ee4e4aa098ba4715a129
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-03-15 22:43:43 +0900

    [Gold IV] Title: 최단경로, Time: 648 ms, Memory: 108004 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int V,E,K;
+    private static int[] dist;
+
+    private static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+
+    private static PriorityQueue<Node> pq = new PriorityQueue<>();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i <= V; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        dist = new int[V+1];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        for(int i = 1; i<=E; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(from).add(new Node(to, cost));
+        }
+
+        dist[K] = 0;
+        pq.add(new Node(K, 0));
+
+        while(!pq.isEmpty()){
+            Node cur =  pq.poll();
+
+            if(dist[cur.to] < cur.cost){
+                continue;
+            }
+
+            for(Node next : graph.get(cur.to)){
+                if(dist[next.to] > dist[cur.to] + next.cost){
+                    dist[next.to] = dist[cur.to] + next.cost;
+                    pq.add(new Node(next.to, dist[next.to]));
+                }
+            }
+        }
+
+        for(int i = 1; i <= V; i++){
+            if(dist[i] == Integer.MAX_VALUE){
+                sb.append("INF").append("\n");
+            }
+            else{
+                sb.append(dist[i]).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+
+    public static class Node implements Comparable<Node> {
+        int to;
+        int cost;
+
+        public Node(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+}
