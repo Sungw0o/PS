f30cad9a532711c5d65975d563cb7e3738875608
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-03-15 22:55:19 +0900

    [Gold V] Title: 최소비용 구하기, Time: 452 ms, Memory: 50768 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int N,M,sCity,eCity;
+
+    private static int[] dist;
+
+    private static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+
+    private static PriorityQueue<Node> pq = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i<=N; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        dist = new int[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        for(int i = 1; i<= M; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(from).add(new Node(to, cost));
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sCity = Integer.parseInt(st.nextToken());
+        eCity = Integer.parseInt(st.nextToken());
+
+        dist[sCity] = 0;
+        pq.add(new Node(sCity, 0));
+
+        while(!pq.isEmpty()){
+
+            Node cur  = pq.poll();
+
+            if(dist[cur.to] < cur.cost ){
+                continue;
+            }
+
+            for(Node next : graph.get(cur.to)){
+                if(dist[next.to] > dist[cur.to] + next.cost){
+                    dist[next.to] = dist[cur.to] + next.cost;
+                    pq.add(new Node(next.to, dist[next.to]));
+                }
+            }
+        }
+        
+        System.out.println(dist[eCity]);
+        br.close();
+    }
+
+
+    public static class Node implements Comparable<Node>{
+        int to,cost;
+
+        public Node(int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+}
