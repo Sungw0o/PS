523b0c35fddc18b0e7a88f5fa8dee53b00bd9a51
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-04-05 11:54:26 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 1668 ms, Memory: 312560 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] tmp;
+    static int K;
+    static int count = 0;
+    static int result = -1;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        K = sc.nextInt();
+
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        tmp = new int[N];
+        mergeSort(A, 0, N - 1);
+
+        System.out.println(result);
+    }
+
+    static void mergeSort(int[] A, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            mergeSort(A, p, q);
+            mergeSort(A, q + 1, r);
+            merge(A, p, q, r);
+        }
+    }
+
+    static void merge(int[] A, int p, int q, int r) {
+        int i = p;
+        int j = q + 1;
+        int t = 0;
+
+        while (i <= q && j <= r) {
+            if (A[i] <= A[j]) {
+                tmp[t++] = A[i++];
+            } else {
+                tmp[t++] = A[j++];
+            }
+        }
+
+        while (i <= q) {
+            tmp[t++] = A[i++];
+        }
+
+        while (j <= r) {
+            tmp[t++] = A[j++];
+        }
+
+        i = p;
+        t = 0;
+        while (i <= r) {
+            count++;
+            if (count == K) {
+                result = tmp[t];
+            }
+            A[i++] = tmp[t++];
+        }
+    }
+}
