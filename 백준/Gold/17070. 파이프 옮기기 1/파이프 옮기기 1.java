ff454a5544a2c7493b624b454708494b98be76bb
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-04-06 17:19:59 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 108 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -3,53 +3,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
-    
-    private static int N, sum;
-    private static int[][] arr;
-    
-    private static final int[] dr = {0, 1, 1};
-    private static final int[] dc = {1, 0, 1};
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        N = Integer.parseInt(br.readLine());
-        sum = 0;
-        arr = new int[N][N];
-        
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        dfs(0, 1, 0);
-        
-        System.out.println(sum);
-    }
-    
-    public static void dfs(int r, int c, int dir) {
-        if (r == N - 1 && c == N - 1) {
-            sum++;
-            return;
-        }
-        
-        for (int i = 0; i < 3; i++) {
-            if ((dir == 0 && i == 1) || (dir == 1 && i == 0)) {
-                continue;
-            }
-            
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-            
-            if (nr >= 0 && nr < N && nc >= 0 && nc < N && arr[nr][nc] == 0) {
-                if (i == 2 && (arr[r][c + 1] == 1 || arr[r + 1][c] == 1)) {
-                    continue;
+public class Main {
+	private static int N,sum;
+	
+	private static int[][] arr;
+	
+	private static int[][][] dp;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N+1][N+1];
+		dp = new int[N+1][N+1][3];
+		sum = 0;
+		
+		for(int i =1 ; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j= 1; j <=  N ; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		dp[1][2][0] = 1;
+		
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 2; j<= N; j++) {
+				
+				if(i==1 && j ==2) continue;
+				
+				if(arr[i][j] == 1) continue;
+				
+				dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+
+                dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+
+                if (arr[i - 1][j] == 0 && arr[i][j - 1] == 0) {
+                    dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
                 }
-                dfs(nr, nc, i);
-            }
-        }
-    }
+			}
+		}
+		
+		sum += dp[N][N][0] + dp[N][N][1] + dp[N][N][2];
+		
+		System.out.println(sum);
+	}
 }
