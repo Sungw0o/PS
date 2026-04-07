05ebb76b2fa897d45c4dcedc52c2511f44bd9f56
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-04-07 13:18:50 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -5,65 +5,73 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
-	
-	private static int[][] maze;
-	private static boolean[][] visited;
-	private static int cnt = 0;
-	private static final int[] dr = {-1,1,0,0};
-	private static final int[] dc = {0,0,-1,1};
-	private static int N,M;
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main{
+	 private static int N,M,time;
+	 
+	 private static int[][] map;
+	 private static boolean[][] visited;
+	 
+	 private static final int[] dr = {-1,1,0,0};
+	 private static final int[] dc = {0,0,-1,1};
+	 
+	 private static Queue<Node> q = new LinkedList<>();
+	 public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		maze = new int[N][M];
-		visited = new boolean[N][M];
 		
-		for(int i = 0; i<N ; i++) {
-			String line = br.readLine();
+		map = new int[N][M];
+		visited = new boolean[N][M];
+		time = 0;
+		
+		for(int i = 0; i < N; i++) {
+			String input = br.readLine();
 			for(int j = 0; j < M; j++) {
-				maze[i][j] = line.charAt(j) - '0';
+				map[i][j] = input.charAt(j) - '0';
 			}
 		}
 		
-		bfs(0,0);
-		System.out.println(maze[N-1][M-1]);
-		br.close();
+		q.add(new Node(0,0,0));
 		
-	}
-	
-	public static void bfs(int sR,int sC) {
-		
-		Queue<int[]> queue = new LinkedList<>();
-		
-		queue.offer(new int[] {sR,sC});
-		visited[sR][sC] = true;
-		
-		while(!queue.isEmpty()) {
-			
-			int[] curr =queue.poll();
-			int r = curr[0];
-			int c = curr[1];
-			
-			for(int i = 0; i< 4;i++) {
-				int nr = r + dr[i];
-				int nc = c + dc[i];
-				
-				if(nr>= 0 && nr < N && nc >= 0 && nc < M) {
-					if(maze[nr][nc] == 1 && !visited[nr][nc]) {
-						visited[nr][nc] = true;
-						maze[nr][nc] = maze[r][c] + 1; 
-	                    queue.offer(new int[] {nr, nc});
-					}
+		while (!q.isEmpty()) {
+
+			Node cur = q.poll();
+
+			if (cur.r == N - 1 && cur.c== M - 1) {
+				time = cur.time;
+				break;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int tr = cur.r + dr[i];
+				int tc = cur.c + dc[i];
+				if (rangeCheck(tr, tc)) {
+					visited[tr][tc] = true;
+					q.add(new Node(tr, tc, cur.time + 1));
 				}
-			}			
+			}
+		}
+
+		System.out.println(time+1);
+		br.close();
+	}
+	 
+	public static boolean rangeCheck(int r, int c) {
+			return r >= 0 && r < N && c >= 0 && c < M && !visited[r][c] && map[r][c] == 1;
+	}
+	 
+	 
+	public static class Node{
+		int r,c,time ;
+		
+		public Node(int r, int c,int time) {
+			this.r = r;
+			this.c = c;
+			this.time = time;
 		}
 	}
 }
