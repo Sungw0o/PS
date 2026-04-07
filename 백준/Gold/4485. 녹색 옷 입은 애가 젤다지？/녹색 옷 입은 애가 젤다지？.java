165eeecf1478953bfd21ed0b76b69ab57d089a92
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-04-07 13:48:04 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 208 ms, Memory: 18376 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -6,94 +6,89 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    
-    private static int N;
-    
-    private static int[] dr = {-1, 1, 0, 0};
-    private static int[] dc = {0, 0, -1, 1}; 
-    
-    private static int[][] maze;
-    private static int[][] dist;
-    
-    public static void main(String[] args) throws NumberFormatException, IOException {        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int problemIdx = 1;
-        
-        while(true) {
-            String line = br.readLine();
-            if (line == null || line.isEmpty()) break; 
-            
-            N = Integer.parseInt(line);
-            if(N == 0) break;
-            
-            init(N);
-            
-            for(int i = 0; i < N; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < N; j++) {
-                    maze[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-            
-            dijkstra();
-            
-            sb.append("Problem ").append(problemIdx++).append(": ")
-              .append(dist[N-1][N-1]).append("\n");
-        }
-        
-        System.out.println(sb);
-        br.close();
-    }
-    
-    public static void init(int n) {
-        maze = new int[n][n];
-        dist = new int[n][n];
-        
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-    }
-    
-    public static void dijkstra() {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        
-        dist[0][0] = maze[0][0];
-        pq.offer(new Node(0, 0, maze[0][0]));
-        
-        while(!pq.isEmpty()) {
-            Node current = pq.poll();
-            
-            if(dist[current.r][current.c] < current.cost) continue;
-            
-            for(int i = 0; i < 4; i++) {
-                int nr = current.r + dr[i];
-                int nc = current.c + dc[i];
-                
-                if(nr >= 0 && nc >= 0 && nr < N && nc < N) {
-                    int nextCost = current.cost + maze[nr][nc];
-                    
-                    if(dist[nr][nc] > nextCost) {
-                        dist[nr][nc] = nextCost;
-                        pq.offer(new Node(nr, nc, nextCost));
-                    }
-                }
-            }
-        }
-    }
-    
-    public static class Node implements Comparable<Node> {
-        int r, c, cost;
-        
-        public Node(int r, int c, int cost) {
-            this.r = r;
-            this.c = c;
-            this.cost = cost;
-        }
-        
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.cost, o.cost);
-        }
-    }
+	
+	private static int N, t;
+	private static int[][] map, dist;
+	
+	private static final int[] dr = {-1, 1, 0, 0};
+	private static final int[] dc = {0, 0, -1, 1};
+	
+	private static PriorityQueue<Node> pq;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		t = 1;
+		while(true) {
+			
+			N = Integer.parseInt(br.readLine());
+			
+			if(N == 0) {
+				break;
+			}
+			
+			dist = new int[N][N];
+			map = new int[N][N];
+			
+			for(int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			for(int i = 0; i < N; i++) {
+				Arrays.fill(dist[i], Integer.MAX_VALUE);
+			}
+			dist[0][0] = map[0][0];
+			
+			pq = new PriorityQueue<>();
+			pq.add(new Node(0, 0, map[0][0]));
+			
+			while(!pq.isEmpty()) {
+				Node cur = pq.poll();
+				
+				if(cur.cost > dist[cur.r][cur.c]) continue;
+				
+				for(int i = 0; i < 4; i++) {
+					int nr = cur.r + dr[i];
+					int nc = cur.c + dc[i];
+					
+					if(rangeCheck(nr,nc)) {
+						int nextCost = cur.cost + map[nr][nc];
+						
+						if(dist[nr][nc] > nextCost) {
+							dist[nr][nc] = nextCost;
+							pq.add(new Node(nr, nc, nextCost));
+						}
+					}
+				}
+			}
+			
+			sb.append("Problem ").append(t++).append(": ").append(dist[N-1][N-1]).append("\n");
+		}
+		
+		System.out.println(sb);
+		br.close();
+	}
+	
+	public static boolean rangeCheck(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < N;
+	}
+	
+	public static class Node implements Comparable<Node>{
+		int r, c, cost;
+		
+		public Node(int r, int c, int cost) {
+			this.r = r;
+			this.c = c;
+			this.cost = cost;
+		}
+		
+		@Override
+		public int compareTo(Node n) {
+			return Integer.compare(this.cost, n.cost);
+		}
+	}
 }
