17882f4bb04c08f9a6556b24c96c5468454f43a6
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-04-21 16:43:54 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 106 ms, Memory: 28,416 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,75 +1,80 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	private static int T, N, M;
-
+	
+	private static int T,N,M,cnt;
+	
 	private static int[] parent;
-
-	public static void main(String[] args) throws IOException {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
 		T = Integer.parseInt(br.readLine());
-
-		for (int t = 1; t <= T; t++) {
+		
+		
+		for(int t= 1; t<= T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
+			
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-
-			parent = new int[N + 1];
-
+			cnt = N;
+			
 			makeSet(N);
-
-			for (int i = 0; i < M; i++) {
+			
+			for(int i =0 ;i < M; i++) {
 				st = new StringTokenizer(br.readLine());
+				
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				union(a, b);
-			}
-
-			int cnt = 0;
-			for (int i = 1; i <= N; i++) {
-				if (parent[i] == i) {
-					cnt++;
+				
+				if(union(a, b)) {
+					cnt--; 
 				}
 			}
+			
 			sb.append("#").append(t).append(" ").append(cnt).append("\n");
+			
+			
 		}
-
+		
 		System.out.println(sb);
 		br.close();
+		
 	}
-
+	
+	
 	public static void makeSet(int n) {
-		parent = new int[n + 1];
-
-		for (int i = 1; i <= n; i++) {
+		
+		parent = new int[n+1];
+		for(int i = 1; i<= n; i++){
 			parent[i] = i;
 		}
 	}
-
+	
 	public static int find(int x) {
-
-		if (parent[x] == x) {
+		
+		if(parent[x] == x) {
 			return x;
-
 		}
-
+		
 		return parent[x] = find(parent[x]);
 	}
-
+	
 	public static boolean union(int a, int b) {
 		int rootA = find(a);
 		int rootB = find(b);
-
-		if (rootA == rootB) {
+		
+		if(rootA== rootB) {
 			return false;
 		}
-
+		
 		parent[rootB] = rootA;
 		return true;
 	}
+
 }
