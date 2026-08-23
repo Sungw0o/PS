e8f1464828229132551568f2f5748affae182b4b
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-08-23 20:43:05 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.78 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 118 MB, 시간: 56.33 ms
+메모리: 115 MB, 시간: 47.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 19일 09:08:53
+2026년 08월 23일 20:43:02
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,21 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
 
 public class Solution {
-	public int[] solution(int[] arr) throws IOException {
-		Stack<Integer> stack = new Stack<>();
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		for (int i : arr) {
-			if(stack.isEmpty() || !stack.peek().equals(i)) {
-				stack.push(i);
-			}
-		}
-		
-		return stack.stream().mapToInt(i -> i).toArray();
-	}
-
+    public int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int num : arr) {
+            // 스택이 비어있거나, 이전 숫자와 다를 때만 push
+            if (stack.isEmpty() || stack.peek() != num) {
+                stack.push(num);
+            }
+        }
+        
+        // 스택 크기만큼 배열 생성
+        int[] answer = new int[stack.size()];
+        
+        // Stack은 LIFO이므로 뒤에서부터 채우기 (또는 get(i) / 향상된 for문 활용)
+        for (int i = answer.length - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
+        
+        return answer;
+    }
 }
